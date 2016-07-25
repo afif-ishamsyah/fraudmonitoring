c684bf1da397fb1fb0a40f45d0779216eec77d46
setiyo <setiyoadiwicaksono@yahoo.com>
2016-07-25 17:16:58 +0700

    perubahan 3

--- fraudmonitoring/resources/views/home_admin.blade.php
+++ fraudmonitoring/resources/views/home_admin.blade.php
@@ -118,11 +118,7 @@
              <!-- DONUT CHART -->
               <div class="box box-danger">
                 <div class="box-header with-border">
-                  <h3 class="box-title">Donut Chart</h3>
-                  <div class="box-tools pull-right">
-                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                  </div>
+                  <h3 class="box-title">Case Status</h3>
                 </div>
                 <div class="box-body chart-responsive">
                   <div class="chart" id="sales-chart" style="height: 250px; position: relative;"></div>
