853fbf83e0d5534a8f06c6117465f39886a44d9f
minymanz <afifhan95@gmail.com>
2016-07-26 17:54:34 +0700

    database

--- fraudmonitoring.sql
+++ fraudmonitoring.sql
@@ -37,7 +37,7 @@ CREATE TABLE `activity` (
 
 /*Data for the table `activity` */
 
-insert  into `activity`(`id_case`,`activity_number`,`activity_date`,`description`,`input_date`,`filename`,`mime`,`original_filename`) values ('8ed14aea-fb89-4fc8-80ea-7f5400ec92ad',6,'2016-06-20','Blokiiir','2016-07-25','php5CBD.tmp.png','image/png','8.png');
+insert  into `activity`(`id_case`,`activity_number`,`activity_date`,`description`,`input_date`,`filename`,`mime`,`original_filename`) values ('8ed14aea-fb89-4fc8-80ea-7f5400ec92ad',6,'2016-06-20','Blokiiir','2016-07-25','php5CBD.tmp.png','image/png','8.png'),('73c50273-25f4-4ae3-b132-e768687ba74d',4,'2016-07-23','Menunggu Respon','2016-07-26','php70CE.tmp.png','image/png','98.png');
 
 /*Table structure for table `activity_parameter` */
 
@@ -60,11 +60,11 @@ DROP TABLE IF EXISTS `case`;
 
 CREATE TABLE `case` (
   `id_case` varchar(100) NOT NULL,
-  `telephone_number` varchar(15) DEFAULT NULL,
-  `case_parameter` int(11) DEFAULT NULL,
-  `case_time` date DEFAULT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  `evidence` blob,
+  `telephone_number` varchar(15) DEFAULT NULL COMMENT 'nomor telpon',
+  `case_parameter` int(11) DEFAULT NULL COMMENT 'jenis case',
+  `case_time` date DEFAULT NULL COMMENT 'waktu case terjadi',
+  `description` varchar(500) DEFAULT NULL COMMENT 'deskripsi',
+  `evidence` blob COMMENT 'bukti',
   `status` char(1) DEFAULT '0',
   `destination` varchar(100) DEFAULT NULL,
   `destination_number` varchar(15) DEFAULT NULL,
@@ -82,7 +82,7 @@ CREATE TABLE `case` (
 
 /*Data for the table `case` */
 
-insert  into `case`(`id_case`,`telephone_number`,`case_parameter`,`case_time`,`description`,`evidence`,`status`,`destination`,`destination_number`,`duration`,`number_of_call`,`input_date`,`filename`,`mime`,`original_filename`) values ('76034255-9401-4fb8-8457-b93174ed6928','08229987096',1,'2016-05-18','completed',NULL,'0','Albania Barat','5343534534',7200,30,'2016-07-25','php9201.tmp.jpg','image/jpeg','12.jpg'),('8ed14aea-fb89-4fc8-80ea-7f5400ec92ad','081383112806',2,'2016-06-20','Parah',NULL,'1','Indonesia','0314666789',3600,600,'2016-07-25','php9678.tmp.jpg','image/jpeg','4.jpg');
+insert  into `case`(`id_case`,`telephone_number`,`case_parameter`,`case_time`,`description`,`evidence`,`status`,`destination`,`destination_number`,`duration`,`number_of_call`,`input_date`,`filename`,`mime`,`original_filename`) values ('35d9ff51-4c0e-49ce-a3fb-20f3201b916f','081383112806',2,'2016-07-20','lagi lagi',NULL,'0','Jepang','42342342342',3600,30,'2016-07-25','php43AD.tmp.jpg','image/jpeg','96.jpg'),('73c50273-25f4-4ae3-b132-e768687ba74d','08145678990',3,'2015-07-20','Kejadian Pertama',NULL,'0','SIngapura','0923565432',3600,20,'2016-07-26','php6B3C.tmp.jpg','image/jpeg','99.jpg'),('76034255-9401-4fb8-8457-b93174ed6928','08229987096',1,'2016-05-18','completed',NULL,'0','Albania Barat','5343534534',7200,30,'2016-07-25','php9201.tmp.jpg','image/jpeg','12.jpg'),('8ed14aea-fb89-4fc8-80ea-7f5400ec92ad','081383112806',2,'2016-06-20','Parah',NULL,'1','Indonesia','0314666789',3600,600,'2016-07-25','php9678.tmp.jpg','image/jpeg','4.jpg');
 
 /*Table structure for table `case_parameter` */
 
@@ -92,7 +92,7 @@ CREATE TABLE `case_parameter` (
   `id_parameter` int(11) NOT NULL AUTO_INCREMENT,
   `description` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id_parameter`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
 /*Data for the table `case_parameter` */
 
@@ -105,11 +105,12 @@ DROP TABLE IF EXISTS `profile`;
 CREATE TABLE `profile` (
   `id_case` varchar(100) DEFAULT NULL,
   `telephone_number` varchar(15) DEFAULT NULL,
+  `main_number` varchar(15) DEFAULT NULL,
   `nipnas` varchar(30) DEFAULT NULL,
   `customer` varchar(100) DEFAULT NULL,
   `nikam` varchar(30) DEFAULT NULL,
   `am` varchar(100) DEFAULT NULL,
-  `telephone_am` varchar(15) DEFAULT NULL,
+  `installation` varchar(100) DEFAULT NULL,
   `segment` char(3) DEFAULT NULL,
   `revenue` int(11) DEFAULT NULL,
   KEY `id_case` (`id_case`),
@@ -118,7 +119,7 @@ CREATE TABLE `profile` (
 
 /*Data for the table `profile` */
 
-insert  into `profile`(`id_case`,`telephone_number`,`nipnas`,`customer`,`nikam`,`am`,`telephone_am`,`segment`,`revenue`) values ('8ed14aea-fb89-4fc8-80ea-7f5400ec92ad','081383112806',NULL,NULL,NULL,NULL,NULL,NULL,NULL),('76034255-9401-4fb8-8457-b93174ed6928','08229987096',NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert  into `profile`(`id_case`,`telephone_number`,`main_number`,`nipnas`,`customer`,`nikam`,`am`,`installation`,`segment`,`revenue`) values ('8ed14aea-fb89-4fc8-80ea-7f5400ec92ad','081383112806','081383112899','51122115','PT Tunggangjaya','21155112','Yak','Surabaya','DBM',40000000),('76034255-9401-4fb8-8457-b93174ed6928','08229987096','08229987099','51133115','Bank BRI','31155113','Ampas','Semarang','DMS',300000),('35d9ff51-4c0e-49ce-a3fb-20f3201b916f','081383112806','081383112899','9119119','PT Indra Jaya Tbk','987789','Isis','Jakarta','FRS',3000000),('73c50273-25f4-4ae3-b132-e768687ba74d','08145678990','08145678999','8900098','Institut Lele','65544556','Surya ','Kejawan','BFS',50000);
 
 /*Table structure for table `profileuser` */
 
@@ -135,7 +136,7 @@ CREATE TABLE `profileuser` (
 
 /*Data for the table `profileuser` */
 
-insert  into `profileuser`(`id`,`username`,`password`,`previledge`,`remember_token`) values (1,'minyman','$2y$10$5nwGenxxxEu2Ggl5IKrIUur0diRxhKl8ROpvYEjYtKkmpZi7WTC9S','1','K5Eg0waIr3u0O98MwFeRwqGgl7C0tlkctBSTEUexWRZVm0Jalzu9fNXFx29D'),(2,'asbun','$2y$10$WlMPY6oqr9aV6A8.x/Ez6uZ6i7DlFC6yjQuPt3ClwEZSpPjv9EpBq','0','XCfVe7mkReqEzKhlI4Vk7lausO1H5leE8j4OW0ZhphZIcZI7y1DvNjIVPnFT');
+insert  into `profileuser`(`id`,`username`,`password`,`previledge`,`remember_token`) values (1,'minyman','$2y$10$5nwGenxxxEu2Ggl5IKrIUur0diRxhKl8ROpvYEjYtKkmpZi7WTC9S','1','TiZ2yMykhBVfCNpZN1wPj6WacHIG1zZw93qEqaeW3DN6NqvXDh1wAFwzdH8e'),(2,'asbun','$2y$10$WlMPY6oqr9aV6A8.x/Ez6uZ6i7DlFC6yjQuPt3ClwEZSpPjv9EpBq','0','dEXe3tu2gOXrHDmj4ot2rImKYZmbr7pnRUycmOyQ74irfjYmfIA0VFd4CvfY');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
